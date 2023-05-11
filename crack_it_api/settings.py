be19c8d2025e84d522de0d6278bc9d63bb14eac7
alcl2000 <amy.555.lewis@gmail.com>
2023-05-11 16:20:06 +0000

    fix: hard coded secret_key removed

--- crack_it_api/settings.py
+++ crack_it_api/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os 
+if os.path.isfile('env.py'):
+    import env
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-_f$re0m3q--g&)1*x=6d)*=b4%n_=ynu8qg1-m40o9o=cerqw)'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
