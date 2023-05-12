27a1180b7cb8451afed9389214b2017051f4cb41
alcl2000 <amy.555.lewis@gmail.com>
2023-05-12 16:44:21 +0000

    add: project model

--- projects/models.py
+++ projects/models.py
@@ -1,3 +1,41 @@
 from django.db import models
+from django.contrib.models import User
 
-# Create your models here.
+
+class Projects(models.Model):
+    """Creates the project model for the proect app"""
+    # progress status
+    status_choices = [
+        ('todo', 'To do'),
+        ('in_progress', 'In Progress'),
+        ('finished', 'Finished')
+    ]
+    # colour choices
+    project_color_choices = [
+        ('red', 'Red'),
+        ('orange', 'Orange'),
+        ('yellow', 'Yellow'),
+        ('green', 'Green'),
+        ('blue', 'Blue'),
+        ('purple', 'Purple'),
+    ]
+    # creation fields
+    owner = models.ForiegnKey(User, on_delete=models.CASCADE)
+    created_on = models.DateTimeField(auto_now_add=True)
+    # user edited fields
+    title = models.CharField(max_length=50)
+    goal_1 = models.CharField(max_length=250)
+    goal_2 = models.CharField(max_length=250)
+    goal_3 = models.CharField(max_length=250)
+    deadline = models.DateTimeField(null=True)
+    status = models.CharField(
+        max_length=11,
+        default=null,
+        choices=status_choices
+    )
+    color = models.CharField(
+            max_length=6,
+            default=null,
+            choices=project_color_choices
+                            )
+    
