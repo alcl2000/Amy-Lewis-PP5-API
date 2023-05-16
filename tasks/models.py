e80aadad7177b9a4e5b9d0b92869b0a8fc21d409
alcl2000 <amy.555.lewis@gmail.com>
2023-05-16 14:41:38 +0000

    add: tasks models

--- tasks/models.py
+++ tasks/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from django.contrib.auth.models import User
+from projects.models import Projects
+
 
 # Create your models here.
+class Tasks(models.Model):
+    progress_options = [
+        ('not_started', 'Not Started'),
+        ('in_progress', 'In Progress'), 
+        ('complete', 'Complete')
+    ]
+    user = models.ForeignKey(User, 
+                             on_delete=models.SET_NULL, 
+                             blank=True,
+                             null=True
+                             )
+    project = models.ForeignKey(Projects, 
+                                on_delete=models.CASCADE,
+                                related_name='tasks',
+                                blank=True,
+                                null=True
+                                )
+    important = models.BooleanField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    due_date = models.DateTimeField(null=True)
+    progress = models.CharField(max_length=11, 
+                                default='not_started', 
+                                choices=progress_options
+                                )
+    # assets = models.FileField()
