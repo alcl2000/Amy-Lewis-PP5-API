d8caa7b50992d1b4cc7deb0b6335da92d82dc364
alcl2000 <amy.555.lewis@gmail.com>
2023-05-16 16:38:59 +0000

    add: task serializer

---
+++ tasks/serializer.py
@@ -0,0 +1,18 @@
+from tasks.models import Tasks
+from rest_framework import serializers
+
+
+class TaskSerializer(serializers.ModelField):
+    user = serializers.ReadOnlyField(source=owner.username)
+    is_owner = serializers.SerializerMethodField()
+
+    def get_owner_id(self, obj):
+        request = self.context['request']
+        return request.user == obj.owner
+
+    class Meta:
+        model = Tasks
+        fields = [
+            'id', 'user', 'is_owner', 'title', 'progress',
+            'due_date', 'created_on', 'important'
+        ]
