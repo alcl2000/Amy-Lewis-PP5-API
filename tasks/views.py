f6e2e5898b712de28b0fed91eb8a17cf1289819c
alcl2000 <amy.555.lewis@gmail.com>
2023-05-17 13:24:00 +0000

    add: task list view

--- tasks/views.py
+++ tasks/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
-
+from models.py import Tasks
+from django_filters.rest_framework import DjangoFilterBackend
+from serializer.py import TaskSerializer
+from rest_framework import generics, permissions, filters
 # Create your views here.
+
+
+class TaskList(generics.ListCreateAPIView):
+    serializer_class = TaskSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    queryset = Tasks.objects.all().orderby('-created_on')
+    # filterset_backends = [ 
+    #     filters.SearchFilter,
+    #     DjangoFilterBackend
+    # ]
+    # filterset_fields = [
+        
+    # ]
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
