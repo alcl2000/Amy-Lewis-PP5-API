c74fc822d3aa5cac8566d414d4753450fe20be90
alcl2000 <amy.555.lewis@gmail.com>
2023-05-18 10:25:14 +0000

    add: task creation test passing

--- tasks/tests.py
+++ tasks/tests.py
@@ -1,3 +1,15 @@
-from django.test import TestCase
+from rest_framework.test import APITestCase
+from rest_framework import status
+from tasks.models import Tasks
+from django.contrib.auth.models import User
 
-# Create your tests here.
+
+class TaskListViewTest(APITestCase):
+    def setUp(self):
+        User.objects.create_user(username='adam', password='123')
+    
+    def test_user_can_create_tasks(self):
+        adam = User.objects.get(username='adam')
+        Tasks.objects.create(title='task 1', important=False)
+        response = self.client.get('/tasks/')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
