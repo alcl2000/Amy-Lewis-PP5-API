1fee13274428be0e17acfc7dd18e6f06f4d76d5f
alcl2000 <amy.555.lewis@gmail.com>
2023-05-19 14:21:26 +0000

    add: logged in user test

--- tasks/tests.py
+++ tasks/tests.py
@@ -8,8 +8,16 @@ class TaskListViewTest(APITestCase):
     def setUp(self):
         User.objects.create_user(username='adam', password='123')
     
-    def test_user_can_retrieve_tasks(self):
+    def test_user_can_retrieve_task_list(self):
         adam = User.objects.get(username='adam')
         Tasks.objects.create(title='task 1', important=False)
         response = self.client.get('/tasks/')
         self.assertEqual(response.status_code, status.HTTP_200_OK)
+    
+    def test_logged_in_user_can_create_tasks(self):
+        self.client.login(username='adam', password='123')
+        response = self.client.post('/tasks/', {'title': 'a title'})
+        count = Tasks().objects.count()
+        self.assertEqual(count, 1)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
