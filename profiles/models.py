a154c27c4e8fd69d10e829ad650ceb147c72237e
alcl2000 <amy.555.lewis@gmail.com>
2023-05-23 17:23:55 +0000

    add: profile model started

--- profiles/models.py
+++ profiles/models.py
@@ -1,3 +1,12 @@
 from django.db import models
-
+from django.contrib.auth.models import User 
+from django.db.models.signals import post_save
 # Create your models here.
+
+
+class Profile(models.Model):
+    owner = models.OneToOneField(User, on_delete=models.CASCADE)
+    created_on = models.DateTimeField(auto_now_add=True)
+    name = models.CharField(max_length=250, blank=True)
+    bio = models.TextField(blank=True)
+
