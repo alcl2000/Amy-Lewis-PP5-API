296dd5bc9dd6c5e4a713fea6ef649f79ea8dca73
alcl2000 <amy.555.lewis@gmail.com>
2023-05-24 15:29:38 +0000

    add: cloudinary install

--- crack_it_api/settings.py
+++ crack_it_api/settings.py
@@ -15,6 +15,13 @@ import os
 if os.path.isfile('env.py'):
     import env
 
+CLOUDINARY_STORAGE = {
+    'CLOUDINARY_URL': os.environ.get('CLOUDINARY_URL')
+}
+MEDIA_URL = '/media/'
+DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
+
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -39,7 +46,9 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    'cloudinary_storage',
     'django.contrib.staticfiles',
+    'cloudinary',
     'rest_framework',
     'django_filters',
     'profiles',
