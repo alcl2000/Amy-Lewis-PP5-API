90b664af5d5ee631d794f146ca566f61e710b318
alcl2000 <amy.555.lewis@gmail.com>
2023-05-25 11:43:57 +0000

    fix: database url fixed

--- crack_it_api/settings.py
+++ crack_it_api/settings.py
@@ -109,14 +109,18 @@ WSGI_APPLICATION = 'crack_it_api.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': ({
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    } if 'DEV' in os.environ else dj_database_url.parse(
-        os.environ.get('DATABASE_URL')
-    ))
-}
+if 'DEV' in os.environ:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
+else:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
+    }
+    print('connected')
 
 
 # Password validation
