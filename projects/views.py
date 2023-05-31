0eb201abd9468c08a502a0a908e7265226f7e039
alcl2000 <amy.555.lewis@gmail.com>
2023-05-31 12:54:57 +0000

    add: filters and search to project views

--- projects/views.py
+++ projects/views.py
@@ -2,12 +2,24 @@ from django.shortcuts import render
 from .models import Projects
 from .serializer import ProjectSerializer
 from crack_it_api.permissions import IsOwnerOrReadOnly
-from rest_framework import generics
+from rest_framework import generics, filters
+from django_filters.rest_framework import DjangoFilterBackend
 
 
 class ProjectList(generics.ListCreateAPIView):
     queryset = Projects.objects.order_by('created_on')
     serializer_class = ProjectSerializer
+    filter_backends = [
+        DjangoFilterBackend,
+        filters.SearchFilter
+    ]
+    filterset_fields = [
+        'owner'
+    ]
+    search_fields = [
+        'title',
+        'owner'   
+    ]
 
 
 class ProjectDetail(generics.RetrieveUpdateDestroyAPIView):
