d61a76647d0a366ae9f823d2968f9b1368367a3e
alcl2000 <amy.555.lewis@gmail.com>
2023-06-01 15:24:04 +0000

    fix: task retrieve test passing

--- tasks/tests.py
+++ tasks/tests.py
@@ -10,11 +10,10 @@ class TaskListViewTest(APITestCase):
         adam = User.objects.create_user(username='adam', password='123')
         Projects.objects.create(id=1, title='project_1', owner=adam)
     
-    # def test_user_can_retrieve_task_list(self):
-    #     adam = User.objects.get(username='adam')
-    #     Tasks.objects.create(title='task 1', important=False)
-    #     response = self.client.get('/tasks/')
-    #     self.assertEqual(response.status_code, status.HTTP_200_OK)
+    def test_user_can_retrieve_task_list(self):
+        adam = User.objects.get(username='adam')
+        response = self.client.get('/tasks/')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
     
     def test_logged_in_user_can_create_tasks(self):
         self.client.login(username='adam', password='123')
