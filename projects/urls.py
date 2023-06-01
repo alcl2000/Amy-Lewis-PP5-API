31fc073aa629de17c9ea916766d0c86975e88cd9
alcl2000 <amy.555.lewis@gmail.com>
2023-06-01 15:59:50 +0000

    fix: task detail and project detail urls fixed

--- projects/urls.py
+++ projects/urls.py
@@ -3,5 +3,5 @@ from projects import views
 
 urlpatterns = [
     path('projects/', views.ProjectList.as_view()),
-    path('profiles/<int:pk>/', views.ProjectDetail.as_view())
+    path('projects/<int:pk>/', views.ProjectDetail.as_view())
 ]
--- tasks/urls.py
+++ tasks/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from tasks import views
 
 urlpatterns = [
-    path('tasks/', views.TaskList.as_view())
+    path('tasks/', views.TaskList.as_view()),
+    path('tasks/<int:pk>', views.TaskDetail.as_view())
 ]
