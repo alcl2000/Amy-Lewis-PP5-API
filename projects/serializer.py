9f3018ba29a800d07d1b57371d3df138af1b6cf9
alcl2000 <amy.555.lewis@gmail.com>
2023-06-06 17:10:10 +0000

    fix: is_member method repaired

--- projects/serializer.py
+++ projects/serializer.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
 from projects.models import Projects
 from profiles.serializer import ProfileSerializer
+from profiles.models import Profile
 from tasks.models import Tasks
 
 
@@ -9,8 +10,8 @@ class ProjectSerializer(serializers.ModelSerializer):
     is_owner = serializers.SerializerMethodField()
     owner_id = serializers.ReadOnlyField(source='owner.id')
     tasks = serializers.SerializerMethodField()
-    # members = ProfileSerializer(read_only=False, many=True)
-    # is_member = serializers.SerializerMethodField()
+    members = ProfileSerializer(read_only=False, many=True)
+    is_member = serializers.SerializerMethodField()
 
     def get_tasks(self, obj):
         tasks = Tasks.objects.filter(project=obj.id)
@@ -22,15 +23,20 @@ class ProjectSerializer(serializers.ModelSerializer):
     
     def get_is_owner(self, obj):
         request = self.context['request']
-        return request.user == obj.owner
+        return request.user == obj.owner_id
     
-    # def get_is_member(self, obj):
-    #     request = self.context['request']
-    #     return request.user in obj.members
+    def get_is_member(self, obj):
+        request = self.context['request']
+        if request.user.is_authenticated:
+            member = Profile.objects.filter(projects=obj.id,
+                                            owner=request.user.id)
+            return True
+        return False
     
     class Meta:
         model = Projects
         fields = [
             'id', 'owner', 'owner_id', 'is_owner', 'title', 'goal_1', 'goal_2',
-            'goal_3', 'deadline', 'created_on', 'color', 'tasks'
+            'goal_3', 'deadline', 'created_on', 'color', 'tasks', 'members',
+            'is_member'
             ]
