9f53ab400ff7cfb4053b28fb06ed968a780c5ef0
alcl2000 <amy.555.lewis@gmail.com>
2023-06-07 13:08:28 +0000

    add: profile detail url test

--- profiles/tests.py
+++ profiles/tests.py
@@ -16,3 +16,16 @@ class ProfileListViewTest(APITestCase):
         self.client.get('/profiles/')
         count = Profile.objects.count()
         self.assertEqual(count, 1)
+
+
+class ProfileDetailView(APITestCase):
+    def setUp(self):
+        adam = User.objects.create_user(username='adam', password='123')
+    
+    def test_user_can_retrieve_profile_with_valid_id(self):
+        response = self.client.get('/profiles/1/')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+    
+    def test_user_cant_retrieve_profile_with_invalid_id(self):
+        response = self.client.get('/profiles/2/')
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
