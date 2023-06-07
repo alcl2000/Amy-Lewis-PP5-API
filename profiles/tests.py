88173457894dad98e7af0c0f5c8fc425bf1f7895
alcl2000 <amy.555.lewis@gmail.com>
2023-06-07 15:17:46 +0000

    add: user editing profiles test

--- profiles/tests.py
+++ profiles/tests.py
@@ -21,11 +21,20 @@ class ProfileListViewTest(APITestCase):
 class ProfileDetailView(APITestCase):
     def setUp(self):
         adam = User.objects.create_user(username='adam', password='123')
+        brian = User.objects.create_user(username='brian', password='123')
     
     def test_user_can_retrieve_profile_with_valid_id(self):
         response = self.client.get('/profiles/1/')
         self.assertEqual(response.status_code, status.HTTP_200_OK)
     
     def test_user_cant_retrieve_profile_with_invalid_id(self):
-        response = self.client.get('/profiles/2/')
+        response = self.client.get('/profiles/3/')
         self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+    
+    def test_user_can_edit_own_profile(self):
+        self.client.login(username='adam', password='123')
+        response = self.client.put('/profiles/1/', {'bio': 'hello'})
+        profile = Profile.objects.filter(pk=1).first()
+        self.assertEqual(profile.bio, 'hello')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
