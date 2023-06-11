262ed377bed9c8af77ee09170ebf231d1dc77c3e
alcl2000 <amy.555.lewis@gmail.com>
2023-06-11 17:46:33 +0000

    fix: task app validation pass

--- tasks/models.py
+++ tasks/models.py
@@ -4,22 +4,21 @@ from django.contrib.auth import get_user_model
 from projects.models import Projects
 
 
-# Create your models here.
-class Tasks(models.Model): 
+class Tasks(models.Model):
     # set user to a blank array method - stops user from returning null
     def set_no_owner(self):
         return get_user_model().objects.get_or_create(username='')[0]
     progress_options = [
         ('not_started', 'Not Started'),
-        ('in_progress', 'In Progress'), 
+        ('in_progress', 'In Progress'),
         ('complete', 'Complete')
     ]
-    owner = models.ForeignKey(User, 
-                              on_delete=models.SET(set_no_owner), 
+    owner = models.ForeignKey(User,
+                              on_delete=models.SET(set_no_owner),
                               blank=True,
                               )
     title = models.CharField(max_length=250)
-    project = models.ForeignKey(Projects, 
+    project = models.ForeignKey(Projects,
                                 on_delete=models.CASCADE,
                                 related_name='tasks',
                                 null=True
@@ -27,8 +26,8 @@ class Tasks(models.Model):
     important = models.BooleanField()
     created_on = models.DateTimeField(auto_now_add=True)
     due_date = models.DateTimeField(null=True)
-    progress = models.CharField(max_length=11, 
-                                default='not_started', 
+    progress = models.CharField(max_length=11,
+                                default='not_started',
                                 choices=progress_options
                                 )
     # assets = models.FileField()
@@ -38,4 +37,3 @@ class Tasks(models.Model):
 
     def __str__(self):
         return f'${self.id}. ${self.title}'
-        
